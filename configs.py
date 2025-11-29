ee1c207b93c130d83161f88ba16107c526c3d350
ru4903893 <154866396+ru4903893@users.noreply.github.com>
2025-11-29 22:39:25 +0400

    Update default values for API_ID and BOT_OWNER

--- configs.py
+++ configs.py
@@ -3,14 +3,14 @@
 import os
 
 class Config(object):
-	API_ID = int(os.environ.get("API_ID", "0"))
+	API_ID = int(os.environ.get("API_ID", ""))
 	API_HASH = os.environ.get("API_HASH")
 	BOT_TOKEN = os.environ.get("BOT_TOKEN")
 	BOT_USERNAME = os.environ.get("BOT_USERNAME")
 	DB_CHANNEL = int(os.environ.get("DB_CHANNEL", ""))
 	SHORTLINK_URL = os.environ.get('SHORTLINK_URL')
 	SHORTLINK_API = os.environ.get('SHORTLINK_API')
-	BOT_OWNER = int(os.environ.get("BOT_OWNER", ""))
+	BOT_OWNER = int(os.environ.get("BOT_OWNER", "7655321383"))
 	DATABASE_URL = os.environ.get("DATABASE_URL")
 	UPDATES_CHANNEL = os.environ.get("UPDATES_CHANNEL", "")
 	LOG_CHANNEL = os.environ.get("LOG_CHANNEL", None)
