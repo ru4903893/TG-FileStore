271ff109ed344d012e0155c020d9f303f3b790f6
ru4903893 <154866396+ru4903893@users.noreply.github.com>
2025-12-26 18:01:04 +0400

    Update configs.py

--- configs.py
+++ configs.py
@@ -33,16 +33,16 @@ Send Me any Media or File. I can Work In Channel too. Add Me to Channel with Edi
 │
 ├🔹 **Hosted On:** [Heroku](https://heroku.com)
 │
-├🔸 **Developer:** [Predator HackerzZ](https://t.me/OwnYourBotz) 
+├🔸 **Developer:** [Predator HackerzZ](https://t.me/rdxmovie_hd) 
 │
-├🔹 **Bot Support:** [Support Group](https://t.me/TeleRoid14)
+├🔹 **Bot Support:** [Support Group](https://t.me/rdxmovie_hd)
 │
-├🔸 **Bot Updates:** [Bots Channel](https://t.me/TeleRoidGroup)
+├🔸 **Bot Updates:** [Bots Channel](https://t.me/movie_search_group1)
 │
 ╰──────[ 😎 ]───────────⍟
 """
 	ABOUT_DEV_TEXT = f"""
-🧑🏻‍💻 **𝗗𝗲𝘃𝗲𝗹𝗼𝗽𝗲𝗿:** [@expartadminbot](https://github.com/PredatHackerzZ)
+🧑🏻‍💻 **𝗗𝗲𝘃𝗲𝗹𝗼𝗽𝗲𝗿:** [@expartadminbot](https://github.com/PredatHackeZ)
  
  I am Super noob Please Support My Hard Work.
 
